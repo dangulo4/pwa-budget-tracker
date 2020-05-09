9b790d63f6f3837d367328e1b01d0bb73dccf6b1
Daniel Angulo <dangulo4@gmail.com>
2020-05-08 23:42:42 -0700

    offline capability

--- public/db.js
+++ public/db.js
@@ -1,40 +1,43 @@
-const indexedDB =
-  window.indexedDB ||
-  window.mozIndexedDB ||
-  window.webkitIndexedDB ||
-  window.msIndexedDB ||
-  window.shimIndexedDB;
-
 let db;
+// create a new db request for a "budget" database.
 const request = indexedDB.open('budget', 1);
 
-request.onupgradeneeded = ({ target }) => {
-  let db = target.result;
+request.onupgradeneeded = function (event) {
+  const db = event.target.result;
   db.createObjectStore('pending', { autoIncrement: true });
+  // create object store called "pending" and set autoIncrement to true
 };
 
-request.onsuccess = ({ target }) => {
+request.onsuccess = function ({ target }) {
   db = target.result;
+  //Returns the online status of the browser.
 
-  // check if app is online before reading from db
   if (navigator.onLine) {
     checkDatabase();
   }
 };
 
 request.onerror = function (event) {
-  console.log('Woops! ' + event.target.errorCode);
+  console.log(event);
+  // log error here
 };
 
 function saveRecord(record) {
-  const transaction = db.transaction(['pending'], 'readwrite');
-  const store = transaction.objectStore('pending');
+  console.log(record);
 
+  const transaction = db.transaction('pending', 'readwrite');
+  const store = transaction.objectStore('pending');
   store.add(record);
+  // create a transaction on the pending db with readwrite access
+  // access your pending object store
+  // add record to your store with add method.
 }
 
 function checkDatabase() {
-  const transaction = db.transaction(['pending'], 'readwrite');
+  // open a transaction on your pending db
+  // access your pending object store
+  // get all records from store and set to a variable
+  const transaction = db.transaction('pending', 'readwrite');
   const store = transaction.objectStore('pending');
   const getAll = store.getAll();
 
@@ -48,9 +51,7 @@ function checkDatabase() {
           'Content-Type': 'application/json',
         },
       })
-        .then((response) => {
-          return response.json();
-        })
+        .then((response) => response.json())
         .then(() => {
           // delete records if successful
           const transaction = db.transaction(['pending'], 'readwrite');
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const logger = require('morgan');
 const mongoose = require('mongoose');
+const logger = require('morgan');
 const compression = require('compression');
 
 const PORT = process.env.PORT || 3000;
@@ -15,13 +15,24 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-mongoose.connect('mongodb://localhost/budget', {
+// routes
+app.use(require('./routes/api.js'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget', {
   useNewUrlParser: true,
   useFindAndModify: false,
 });
 
-// routes
-app.use(require('./routes/api.js'));
+//Set up Mongo database.
+const databaseUrl = process.env.MONGODB_URI || 'budget';
+const collections = ['budget'];
+
+//Set reference to our database.
+const db = mongojs(databaseUrl, collections);
+
+db.on('error', (error) => {
+  console.log('Database Error:', error);
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
